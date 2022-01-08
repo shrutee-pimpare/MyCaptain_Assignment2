79c4929a196db767271089c9a0e2832928ee6d4a
shrutee-pimpare <90600754+shrutee-pimpare@users.noreply.github.com>
2022-01-08 17:13:23 +0530

    Add files via upload

---
+++ PositiveNumbers.py
@@ -0,0 +1,11 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+print("\nPositive Numbers in List1 are:")
+for i in range (len(list1)):
+    if(list1[i]>0):
+        print(list1[i],end=",")
+
+print("\nPositive Numbers in List2 are:")
+for i in range (len(list2)):
+    if(list2[i]>0):
+        print(list2[i],end=",")
